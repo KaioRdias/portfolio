9c6a1a05e256430054722114a727d1653bec597d
Kaio Dias <kaio.dias@loga.net.br>
2024-11-27 09:41:14 -0300

    Portfólio

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  /*if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatarKaio.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+  }*/
+
+  /*if (html.classList.contains("light")) {
+  } else {
+    img.setAttribute("alt", "Teste foto Mayke")
+  }*/
+}
